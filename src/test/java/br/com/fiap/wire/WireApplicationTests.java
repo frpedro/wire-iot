c8c28f8ee92dd02130defdad02c8076ef5c50c71
Pedro <pedrofernandesctt@gmail.com>
2025-05-08 12:06:34 -0300

    initial commit

---
+++ src/test/java/br/com/fiap/wire/WireApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
