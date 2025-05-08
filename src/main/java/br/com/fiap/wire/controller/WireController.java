a2e0f10d2f723e170d0c7f8ffaffc358fa461414
Pedro <pedrofernandesctt@gmail.com>
2025-05-08 14:06:57 -0300

    configs iniciais e organização do projeto

---
+++ src/main/java/br/com/fiap/wire/controller/WireController.java
@@ -0,0 +1,9 @@
+package br.com.fiap.wire.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class WireController {
+
+}
